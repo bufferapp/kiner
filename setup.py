e0834cfd448d534ec9aef5293f78602e9d733ad7
David Gasquez <davidgasquez@gmail.com>
2018-05-10 11:57:17 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='kiner',
     packages=['kiner'],
-    version='0.5.1',
+    version='0.6.0',
     description='Python AWS Kinesis Producer',
     author='David Gasquez',
     license='MIT',
