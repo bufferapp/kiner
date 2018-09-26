49c2183c3322e65ab49a21fe5cb3a9a0d4c8442f
David Gasquez <davidgasquez@gmail.com>
2018-09-26 08:21:06 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup
 
 setup(
-    name='kiner',
-    packages=['kiner'],
-    version='0.6.2',
-    description='Python AWS Kinesis Producer',
-    author='David Gasquez',
-    license='MIT',
-    author_email='davidgasquez@buffer.com',
-    url='https://github.com/bufferapp/kiner',
-    keywords=['kinesis', 'producer', 'aws'],
-    install_requires=['boto3', 'future']
+    name="kiner",
+    packages=["kiner"],
+    version="0.6.4",
+    description="Python AWS Kinesis Producer",
+    author="David Gasquez",
+    license="MIT",
+    author_email="davidgasquez@buffer.com",
+    url="https://github.com/bufferapp/kiner",
+    keywords=["kinesis", "producer", "aws"],
+    install_requires=["boto3", "future"],
 )
